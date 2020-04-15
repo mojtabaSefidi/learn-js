1141162f3f987d08955d40634ddccb92430e9148
MojTabaSefidi <mohammadali.esfahani99@gmail.com>
2020-04-15 19:24:41 +0430

    adding files and codes that are needed to complete the project

---
+++ forkify/src/js/index.js
@@ -0,0 +1,12 @@
+/*
+ string from "./models/Search"
+// import {
+//     add as a,
+//     multiply as m,
+//     ID as i
+// } from "./views/searchView"
+import * as searchView from "./views/searchView"
+// console.log(`using imported functions : ${add(ID,2)} and ${multiply(5,6)}. and ${string}`);
+// console.log(`using imported functions : ${a(i,2)} and ${m(5,6)}. and ${string}`);
+console.log(`using imported functions : ${searchView.add(searchView.ID,2)} and ${searchView.multiply(5,6)}. and ${string}`);
+*/
